c120d6b354d1b5533421365c9feb7129c243acad
Besthamalla <Sanjit.Besthamalla@philips.com>
2025-04-09 14:44:02 +0530

    update: change csv logic

--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ import pandas as pd
 def load_csv(filepath):
     return pd.read_csv(filepath)
 
-
 def row_to_text(row):
     return (
         f"Order ID {row['OrderID']} is for {row['ProductName']} in the category "
@@ -11,7 +10,6 @@ def row_to_text(row):
         f"Quantity ordered: {row['Quantity']} at ${row['UnitPrice']} per unit. "
         f"Status of delivery: {row['Status']}."
     )
-
 
 def dataframe_to_text_chunks(df):
     return [row_to_text(row) for _, row in df.iterrows()]
